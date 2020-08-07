6e931caaa29b7164c639b14d3f65268f9acfe690
Andrew Bartlett <abartlet@samba.org>
2020-08-07 22:15:38 +1200

    Add script to build everything automatically on centos 7
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>

---
+++ build-centos-7.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# This command only builds CentOS 7, which avoids the problems
+# building Fedora 32 under docker
+
+make MOCKS=samba4repo-7-x86_64 SAMBAPKGS="libtalloc-2.3.x-srpm  libtdb-1.4.x-srpm libtevent-0.10.x-srpm libldb-2.1.x-srpm  compat-nettle32-3.x-srpm compat-gnutls34-3.x-srpm  samba-4.12.x-srpm"
