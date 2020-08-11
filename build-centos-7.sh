8dc63e9f75a08b5c33160131232eef806e26c327
Andrew Bartlett <abartlet@samba.org>
2020-08-11 12:05:04 +1200

    Put prep.sh into build-centos-7.sh
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>

--- build-centos-7.sh
+++ build-centos-7.sh
@@ -3,4 +3,6 @@
 # This command only builds CentOS 7, which avoids the problems
 # building Fedora 32 under docker
 
+$(dirname $0)/prep.sh
+
 make MOCKS=samba4repo-7-x86_64 SAMBAPKGS="libtalloc-2.3.x-srpm  libtdb-1.4.x-srpm libtevent-0.10.x-srpm libldb-2.1.x-srpm  compat-nettle32-3.x-srpm compat-gnutls34-3.x-srpm  samba-4.12.x-srpm"
